57a9bd34622a76a2a4b545fd222c1a2fef7185dc
Ossamoon <osamu950623@gmail.com>
2021-05-31 13:49:15 +0900

    try station 13

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,5 +1,10 @@
-function getData() {
-    return arrowFunction()
-}
+const getData = () => {
+  return arrowFunction();
+};
 
 // Arrowという文字列を返すarrowFunctionという名前のアロー関数を追加してください
+const arrowFunction = () => {
+  return "Arrow";
+};
+
+export { getData };
