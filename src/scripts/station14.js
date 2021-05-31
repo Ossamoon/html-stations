bd806a92f721c0a80f660ea1646bb74e919e26fc
Ossamoon <osamu950623@gmail.com>
2021-05-31 14:23:45 +0900

    try station 14

--- src/scripts/station14.js
+++ src/scripts/station14.js
@@ -25,7 +25,9 @@ const buildFullName = (student) => {
   // Station14の問題はこちらです。想定する答えをここを書いてください。
   // 氏名がわかるようにしつつ、半角スペースで繋いでください。
   // またtest配列のそれぞれのオブジェクトに対して、full_nameのプロパティが追加されるように実装すること
-  student.full_name = student.family_name + " " + student.family_name;
+  const res = { ...student };
+  res.full_name = res.family_name + " " + res.family_name;
+  return res;
 };
 
 // 出力が想定される答え
